9c2faddb9f62062ecbe2ad51b30fb9333d77d018
extermin4tus <montarstrike777@gmail.com>
2023-03-16 23:30:11 +0300

    started declaring a HomeViewModel.kt

--- app/src/main/java/com/example/flightsearchapp/ui/viewModels/HomeViewModel.kt
+++ app/src/main/java/com/example/flightsearchapp/ui/viewModels/HomeViewModel.kt
@@ -1,9 +1,26 @@
 package com.example.flightsearchapp.ui.viewModels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.flightsearchapp.data.database.Favorite
+import com.example.flightsearchapp.data.repository.FlightRepository
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 
-class HomeViewModel : ViewModel() {
+class HomeViewModel(private val flightRepository: FlightRepository) : ViewModel() {
     companion object {
         val TIMEOUT_MILLIS = 5_000L
     }
+
+    val homeUiState: StateFlow<HomeUiState> = flightRepository.getAllFavorites()
+        .map { HomeUiState(favorites = it) }
+        .stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+            HomeUiState(favorites = listOf())
+        )
+
+    suspend fun removeFavorite(favorite: Favorite) = flightRepository.deleteFavorite(favorite)
+
+    suspend fun addToFavorite(favorite: Favorite) = flightRepository.insertFavorite(favorite)
 }
