e4c8b20b1a082fff433b188aad8849b7f36e3f4c
extermin4tus <montarstrike777@gmail.com>
2023-03-16 23:17:19 +0300

    confirmation of previous commit

--- app/src/main/java/com/example/flightsearchapp/data/repository/FlightRepository.kt
+++ app/src/main/java/com/example/flightsearchapp/data/repository/FlightRepository.kt
@@ -1,4 +1,30 @@
 package com.example.flightsearchapp.data.repository
 
+import com.example.flightsearchapp.data.database.Airport
+import com.example.flightsearchapp.data.database.Favorite
+import com.example.flightsearchapp.data.database.FlightDao
+import kotlinx.coroutines.flow.Flow
+
 interface FlightRepository {
+    suspend fun insertFavorite(favorite: Favorite)
+
+    suspend fun deleteFavorite(favorite: Favorite)
+
+    fun getAllFavorites(): Flow<List<Favorite>>
+
+    fun getAirportByIataOrName(searchString: String): Flow<List<Airport>>
+
+    fun getAirportById(id: Int): Flow<Airport>
+}
+
+class OfflineFlightRepository(private val flightDao: FlightDao) : FlightRepository {
+    override suspend fun insertFavorite(favorite: Favorite) = flightDao.insertFavorite(favorite)
+
+    override suspend fun deleteFavorite(favorite: Favorite) = flightDao.deleteFavorite(favorite)
+
+    override fun getAllFavorites(): Flow<List<Favorite>> = flightDao.getAllFavorites()
+
+    override fun getAirportByIataOrName(searchString: String): Flow<List<Airport>> = flightDao.getAirportByIataOrName(searchString)
+
+    override fun getAirportById(id: Int): Flow<Airport> = flightDao.getAirportById(id)
 }
