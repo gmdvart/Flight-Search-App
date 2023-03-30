70949896d2ff9dcac67141928232e55f94a12476
extermin4tus <montarstrike777@gmail.com>
2023-03-30 23:51:17 +0300

    Final version of the Flight Search Application

--- app/src/main/java/com/example/flightsearchapp/data/database/FlightDatabase.kt
+++ app/src/main/java/com/example/flightsearchapp/data/database/FlightDatabase.kt
@@ -4,18 +4,19 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 
-@Database([Airport::class, Favorite::class], version = 1, exportSchema = false)
+@Database([Airport::class, Favorite::class], version = 1)
 abstract class FlightDatabase : RoomDatabase() {
     abstract fun getDao(): FlightDao
-
     companion object {
         @Volatile
         private var INSTANCE: FlightDatabase? = null
 
         fun getDatabase(context: Context): FlightDatabase = INSTANCE ?: synchronized(this) {
             Room.databaseBuilder(context = context, klass = FlightDatabase::class.java, name = "flight_search")
-                .fallbackToDestructiveMigration()
+                .createFromAsset("database/flight_search.db")
                 .build()
                 .also { INSTANCE = it }
         }
